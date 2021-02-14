913e5c5765cf28bbfb1dcef5bfc236501b2514a1
Emrah Demir <demire801@gmail.com>
2021-02-14 11:53:25 +0300

    Some header

--- socks.h
+++ socks.h
@@ -21,4 +21,61 @@ struct sock_buffer_param {
 #define IOCTL_SOCKS_RECV		_IOWR('s', 5, struct sock_buffer_param)
 #define IOCTL_SOCKS_RESIZE		_IOWR('s', 6, uint64_t)
 
+int sock_init(int fd, size_t size) {
+    return ioctl(fd, IOCTL_SOCKS_INIT, size);
+}
+
+int sock_listen(int fd, char *name) {
+    struct sock_name_param param;
+    memset(&param, 0, sizeof(param));
+    strncpy(param.name, name, sizeof(param.name) -1);
+    int err = ioctl(fd, IOCTL_SOCKS_LISTEN, &param);
+    if(err < 0){
+        perror("ioctl sock_listen");
+        fail();
+    }
+    return err;
+}
+
+int sock_connect(int fd, char *name) {
+    struct sock_name_param param;
+    memset(&param, 0, sizeof(param));
+    strncpy(param.name, name, sizeof(param.name) -1);
+    int err =  ioctl(fd, IOCTL_SOCKS_CONNECT, &param);
+    if(err < 0){
+        perror("ioctl sock_connect");
+        fail();
+    }
+    return err;
+}
+
+int sock_send(int fd, void *buffer, size_t size) {
+    struct sock_buffer_param param = { .size = size, .buffer = buffer};
+    int err =  ioctl(fd, IOCTL_SOCKS_SEND, &param);
+    if(err < 0){
+        perror("ioctl sock_send");
+        fail();
+    }
+    return err;
+}
+
+int sock_recv(int fd, void *buffer, size_t size) {
+    struct sock_buffer_param param = { .size = size, .buffer = buffer};
+    int err = ioctl(fd, IOCTL_SOCKS_RECV, &param);
+    if(err < 0){
+        perror("ioctl sock_recv");
+        fail();
+    }
+    return err;
+}
+
+int sock_resize(int fd, size_t size) {
+    int err =  ioctl(fd, IOCTL_SOCKS_RESIZE, size);
+    if(err < 0){
+        perror("ioctl sock_resize");
+        fail();
+    }
+    return err;
+}
+
 #endif
